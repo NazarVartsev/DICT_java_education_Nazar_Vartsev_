8d9c161456d0660893c843619e0974758d562f30
Nazar Vartsev <n.a.vartsev@student.khai.edu>
2021-12-10 03:50:55 +0200

    WordsStorage added method hidingWords

--- src/Hangman/WordsStorage.java
+++ src/Hangman/WordsStorage.java
@@ -1,5 +1,26 @@
 package Hangman;
 
+import java.util.Random;
+
 public class WordsStorage {
-    String [] words_list = {"python", "java", "hot dog", "Mia Khalifa"};
+    private String word;
+    private final String[] words_list = {"python", "java", "hot dog", "Mia Khalifa"};
+    String hidingWords() {
+        String word_dash_out;
+        Random r = new Random();
+        int a_i = r.nextInt(3);
+        word = words_list[a_i];
+        String word_dash = "";
+        String dash = "-";
+        int len_word_r = word.length() - 1;
+        int two_len_word_r = len_word_r - 2;
+        for (int i = 0; i <= two_len_word_r; i++) {
+            word_dash += dash;
+        }
+        return word_dash_out = word.charAt(0) + word.charAt(1) + word_dash;
+    }
+
+    String getWord() {
+        return word;
+    }
 }
