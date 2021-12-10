576856b5d506cedcd9f744fcfce3a7f497d0f9bd
Nazar Vartsev <n.a.vartsev@student.khai.edu>
2021-12-10 03:42:35 +0200

    Hangman {2} stage

--- src/Hangman/Hangman.java
+++ src/Hangman/Hangman.java
@@ -1,8 +1,24 @@
 package Hangman;
 
+import java.util.Objects;
+import java.util.Scanner;
+
 public class Hangman {
     public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        WordsStorage wor = new WordsStorage();
+
         System.out.println("HANGMAN");
         System.out.println("The game will be available soon.");
+
+        System.out.println("Guess the word:");
+        String out_answer = s.nextLine();
+        for (int i = 0; i < 4; i++) {
+            if (Objects.equals(wor.words_list[i], out_answer)) {
+                System.out.println("You survived!");
+                break;
+            }
+        }
+        System.out.println("You lost!");
     }
 }
