097f8f7842ab937d5f29d9b78b9bea9de1b1c2c6
Nazar Vartsev <n.a.vartsev@student.khai.edu>
2021-12-10 03:51:50 +0200

    Hangman {4} stage

--- src/Hangman/Hangman.java
+++ src/Hangman/Hangman.java
@@ -1,33 +1,24 @@
 package Hangman;
-
 import java.util.Objects;
 import java.util.Scanner;
-import java.util.Random;
+
+// Appdata 4 !!!
 
 public class Hangman {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        Random r = new Random();
         WordsStorage wor = new WordsStorage();
-
         // Начало
         System.out.println("HANGMAN");
         System.out.println("The game will be available soon.");
-
         // Проверка введенного слова
         System.out.println("Guess the word:");
-        String out_answer = s.nextLine();
-        for (int i = 0; i < 4; i++) {
-            if (Objects.equals(wor.words_list[i], out_answer)) {
-                System.out.println("You survived!");
-                break;
-            }
-            int a_i = r.nextInt(3);
-            if (Objects.equals(wor.words_list[a_i], out_answer)) {
-                System.out.println("You survived!");
-            } else {
-                System.out.println("You lost!");
-            }
+        String word_dash = wor.hidingWords();
+        System.out.println(word_dash);
+        String word_s = s.nextLine();
+        if (Objects.equals(word_s, wor.getWord())) {
+            System.out.println("You survived!");
+        } else {
             System.out.println("You lost!");
         }
     }
