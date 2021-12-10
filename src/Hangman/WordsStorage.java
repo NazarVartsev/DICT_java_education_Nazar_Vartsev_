615da73de77de47c6eb3c6cddecbb0f8b7003ce7
Nazar Vartsev <n.a.vartsev@student.khai.edu>
2021-12-10 04:04:47 +0200

    Hangman {4} stage

--- src/Hangman/WordsStorage.java
+++ src/Hangman/WordsStorage.java
@@ -5,8 +5,10 @@ import java.util.Random;
 public class WordsStorage {
     private String word;
     private final String[] words_list = {"python", "java", "hot dog", "Mia Khalifa"};
+
     String hidingWords() {
         String word_dash_out;
+
         Random r = new Random();
         int a_i = r.nextInt(3);
         word = words_list[a_i];
@@ -17,7 +19,7 @@ public class WordsStorage {
         for (int i = 0; i <= two_len_word_r; i++) {
             word_dash += dash;
         }
-        return word_dash_out = word.charAt(0) + word.charAt(1) + word_dash;
+        return word_dash_out = "" + word.charAt(0) + word.charAt(1) + word_dash;
     }
 
     String getWord() {
