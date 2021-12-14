5d4bc0234b3c716cbd2c5406eff593c55384641c
Nazar Vartsev <n.a.vartsev@student.khai.edu>
2021-12-14 17:22:59 +0200

    Hangman {7} stage

--- src/Hangman/Hangman.java
+++ src/Hangman/Hangman.java
@@ -21,21 +21,34 @@ public class Hangman {
             System.out.println(wor.getWord_dash());
             String litter_s = s.nextLine();
             char char_s = litter_s.charAt(0);
+            String i_string = "[A-z]";
+            if (litter_s.length() != 1) {
+                System.out.println("You should input a single letter.");
+                continue;
+            } else if (!litter_s.matches(i_string)) {
+                System.out.println("Please enter a lowercase English letter.");
+                continue;
+            }
             wor.openOneLitterWord(char_s);
             if (Objects.equals(wor.getWord_dash(), wor.getWord())) {
                 System.out.println("You survived!");
                 break;
             }
+            // Проверка на правильность введенной буквы
             if (!Objects.equals(word_dash_old, wor.getWord_dash())) {
                 word_dash_old = wor.getWord_dash();
                 continue;
             }
+
+            // Проверка на одну и ту же самую букву и неправильную букву
             boolean i_boolean = wor.checkLitterWord_dash(char_s);
             if (i_boolean) {
-                System.out.println("No improvements");
-                a--;
+                System.out.println("You've already guessed this letter.");
+            } else if (wor.getBooleanWrong_litter(char_s)) {
+                System.out.println("You've already guessed this letter.");
             } else {
-                System.out.println("That letter doesn't appear in the word");
+                wor.appendWrong_litter(char_s);
+                System.out.println("That letter doesn't appear in the word.");
                 a--;
             }
         }
