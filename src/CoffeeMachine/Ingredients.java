ed31f33c910c2381fd1486ceb6849e51808268d2
Nazar Vartsev <n.a.vartsev@student.khai.edu>
2021-12-21 01:20:28 +0200

    Ingredients added examination cups coffee

--- src/CoffeeMachine/Ingredients.java
+++ src/CoffeeMachine/Ingredients.java
@@ -1,19 +1,44 @@
 package CoffeeMachine;
 
 public class Ingredients {
-    private final int water = 200;
-    private final int milk = 50;
-    private final int coffee_beans = 15;
+    private final int waterOneCup = 200;
+    private final int milkOneCup = 50;
+    private final int coffee_beansOneCup = 15;
+    private int water;
+    private int milk;
+    private int coffee_beans;
 
-    String multiplicationIngredientsCups(int cups) {
-        String stringIngredientsDoYouNeed;
+    private boolean multiplicationIngredientsCups(int cups) {
+        int waterMultiplication = waterOneCup * cups;
+        int milkMultiplication = milkOneCup * cups;
+        int coffee_beansMultiplication = coffee_beansOneCup * cups;
+        return waterMultiplication <= water ^ milkMultiplication <= milk ^ coffee_beansMultiplication <= coffee_beans;
+    }
 
-        int waterMultiplication = water * cups;
-        int milkMultiplication = milk * cups;
-        int coffee_beansMultiplication = coffee_beans * cups;
-        return stringIngredientsDoYouNeed = "For " + cups + " cups of coffee you will need:\n"
-                + waterMultiplication + " ml of water\n"
-                + milkMultiplication + " ml of milk\n"
-                + coffee_beansMultiplication + " g of coffee beans\n";
+    private int examinationCupsMake() {
+        int i = 0;
+        while (multiplicationIngredientsCups(i)) {
+            i++;
+        }
+        return i - 1;
+    }
+
+    String examinationCupsCoffee(int cups) {
+        int iCups = examinationCupsMake();
+        int iCupsSurplus = iCups - cups;
+
+        if (iCupsSurplus > 0) {
+            return "Yes, I can make that amount of coffee (and even " + iCupsSurplus + " more than that)";
+        } else if (iCupsSurplus == 0) {
+            return "Yes, I can make that amount of coffee";
+        } else {
+            return "No, I can make only " + iCups + " cups of coffee";
+        }
+    }
+
+    void assignmentIngredients(int water_i, int milk_i, int coffee_beans_i) {
+        water = water_i;
+        milk = milk_i;
+        coffee_beans = coffee_beans_i;
     }
 }
