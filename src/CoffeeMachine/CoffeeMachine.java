4372c3d2df6d86e35cac1f3015f679a7e4b81154
Nazar Vartsev <n.a.vartsev@student.khai.edu>
2021-12-20 23:35:18 +0200

    CoffeeMachine {2} stage

--- src/CoffeeMachine/CoffeeMachine.java
+++ src/CoffeeMachine/CoffeeMachine.java
@@ -1,7 +1,12 @@
 package CoffeeMachine;
 
+import java.util.Scanner;
+
 public class CoffeeMachine {
     public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        Ingredients ingredients = new Ingredients();
+
         System.out.println("""
                 Starting to make a coffee
                 Grinding coffee beans
@@ -10,5 +15,9 @@ public class CoffeeMachine {
                 Pouring coffee into the cup
                 Pouring some milk into the cup
                 Coffee is ready!""");
+
+        System.out.println("Write how many cups of coffee you will need:");
+        int cups = s.nextInt();
+        System.out.println(ingredients.multiplicationIngredientsCups(cups));
     }
 }
