9be54ffb5c1de41879b2729778bfd027b5ffb9d9
Nazar Vartsev <n.a.vartsev@student.khai.edu>
2021-12-26 23:10:43 +0200

    CoffeeMachine {4} stage

--- src/CoffeeMachine/CoffeeMachine.java
+++ src/CoffeeMachine/CoffeeMachine.java
@@ -6,31 +6,32 @@ public class CoffeeMachine {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         CoffeeMachine coffeeMachine = new CoffeeMachine();
+        Ingredients ingredients = new Ingredients();
         String string_s = "";
 
         while (true) {
+            System.out.println("Write action (buy, fill, take, exit):");
+            string_s = s.nextLine();
             if (string_s.equals("buy")) {
-                coffeeMachine.startingCoffeeMachine();
-                string_s = "";
+                ingredients = coffeeMachine.startingCoffeeMachine(ingredients);
             } else if (string_s.equals("fill")) {
-
+                ingredients.fillIngredients();
+                ingredients.printIngredients();
             } else if (string_s.equals("take")) {
-
+                ingredients.takeMoney();
+                ingredients.printIngredients();
             } else if (string_s.equals("exit")) {
                 break;
-            } else {
-                System.out.println("Write action (buy, fill, take, exit):");
-                string_s = s.nextLine();
             }
         }
     }
 
-    void startingCoffeeMachine() {
+    Ingredients startingCoffeeMachine(Ingredients ingredients) {
         Scanner s = new Scanner(System.in);
-        Ingredients ingredients = new Ingredients();
-
         System.out.println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino:");
         int coffee_s = s.nextInt();
-        System.out.println(ingredients.examinationCupsCoffee(coffee_s));
+        ingredients.cookingCoffee(coffee_s);
+        ingredients.printIngredients();
+        return ingredients;
     }
 }
