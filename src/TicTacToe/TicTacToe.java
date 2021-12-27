1b4fc757e1340f2cae226ad14248241be9272d47
Nazar Vartsev <n.a.vartsev@student.khai.edu>
2021-12-27 19:35:20 +0200

    TicTacToe {4} stage

--- src/TicTacToe/TicTacToe.java
+++ src/TicTacToe/TicTacToe.java
@@ -19,6 +19,7 @@ public class TicTacToe {
         int numberX;
         int numberO;
 
+        // Построение поля
         do {
             numberX = 0;
             numberO = 0;
@@ -38,26 +39,44 @@ public class TicTacToe {
             }
         } while (iBooleanXO_);
 
-        char[][] arrayField = ticTacToe.arrayField;
+        // Запрос координат
+        String coordinates;
+        String a;
+        String[] numbersCoordinates;
+        boolean iBooleanFullMatrix = false;
+        while (true) {
+            ticTacToe.printArrayField();
+            System.out.print("Enter the coordinates: ");
+            coordinates = s.nextLine();
+            if (coordinates.matches("\\w*[A-z]\\w*")) {
+                System.out.println("You should enter numbers!");
+                iBooleanFullMatrix = true;
+                continue;
+            } else if (coordinates.length() > 3 | !coordinates.matches("\\w*([1-3]\\s?[1-3])\\w*")) {
+                System.out.println("Coordinates should be from 1 to 3!");
+                continue;
+            }
+            numbersCoordinates = coordinates.split("\\s*(\\s?)\\s*");
+            ticTacToe.toWriteXO(numbersCoordinates);
+            if (iBooleanFullMatrix) {
+                if (ticTacToe.examinationFullMatrix()) {break;}
+                iBooleanFullMatrix = false;
+            }
+        }
+
+        // Проверка игры
+        char[][] arrayFieldMain = ticTacToe.arrayField;
         arrayFieldFlip = ticTacToe.examinationFlipMatrix(arrayFieldFlip);
         boolean booleanNumberXO = numberX - 1 > numberO | numberO - 1 > numberX;
 
-        boolean booleanArrayField1 = ticTacToe.examinationXO(winOrLose, arrayCharXO_[0], arrayField);
-        if (!booleanArrayField1){
+        boolean booleanArrayField1 = ticTacToe.examinationXO(winOrLose, arrayCharXO_[0], arrayFieldMain);
+        if (!booleanArrayField1) {
             booleanArrayField1 = ticTacToe.examinationXO(winOrLose, arrayCharXO_[0], arrayFieldFlip);
         }
-        boolean booleanArrayField2 = ticTacToe.examinationXO(winOrLose, arrayCharXO_[1], arrayField);
+        boolean booleanArrayField2 = ticTacToe.examinationXO(winOrLose, arrayCharXO_[1], arrayFieldMain);
         if (!booleanArrayField2) {
             booleanArrayField2 = ticTacToe.examinationXO(winOrLose, arrayCharXO_[1], arrayFieldFlip);
         }
-
-        System.out.println("-------\n"
-                + "|" + arrayFieldFlip[0][0] + " " + arrayFieldFlip[0][1] + " " + arrayFieldFlip[0][2] + "|\n"
-                + "|" + arrayFieldFlip[1][0] + " " + arrayFieldFlip[1][1] + " " + arrayFieldFlip[1][2] + "|\n"
-                + "|" + arrayFieldFlip[2][0] + " " + arrayFieldFlip[2][1] + " " + arrayFieldFlip[2][2] + "|\n"
-                + "-------");
-
-        System.out.println(booleanDash);
         if (!booleanArrayField1 & !booleanArrayField2 & booleanDash) {
             System.out.println("Game not finished");
         } else if (booleanNumberXO) {
@@ -101,5 +120,40 @@ public class TicTacToe {
             }
         }
         return arrayFieldFlip;
+    }
+
+    void toWriteXO(String[] numbersCoordinates) {
+        String a = numbersCoordinates[0];
+        String b = numbersCoordinates[1];
+        int aInt = Integer.parseInt(a);
+        int bInt = Integer.parseInt(b);
+        if (arrayField[aInt - 1][bInt - 1] == 'X' | arrayField[aInt - 1][bInt - 1] == 'O') {
+            System.out.println("This cell is occupied! Choose another one!");
+        } else if (a.matches("[1-3]") & b.matches("[1-3]")) {
+            arrayField[aInt - 1][bInt - 1] = 'X';
+        } else {
+            System.out.println("Coordinates should be from 1 to 3!");
+        }
+    }
+
+    boolean examinationFullMatrix() {
+        boolean iBoolean = false;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                iBoolean = arrayField[i][j] == 'X' | arrayField[i][j] == 'O';
+                if (!iBoolean) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    void printArrayField() {
+        System.out.println("-------\n"
+                + "|" + arrayField[0][0] + " " + arrayField[0][1] + " " + arrayField[0][2] + "|\n"
+                + "|" + arrayField[1][0] + " " + arrayField[1][1] + " " + arrayField[1][2] + "|\n"
+                + "|" + arrayField[2][0] + " " + arrayField[2][1] + " " + arrayField[2][2] + "|\n"
+                + "-------");
     }
 }
