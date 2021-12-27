de62a7cec4ccc39b2c784cd114e06e02f8d2d5d1
Nazar Vartsev <n.a.vartsev@student.khai.edu>
2021-12-27 12:23:43 +0200

    TicTacToe added matrix on two dimensional array

--- src/TicTacToe/TicTacToe.java
+++ src/TicTacToe/TicTacToe.java
@@ -8,7 +8,8 @@ public class TicTacToe {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         TicTacToe ticTacToe = new TicTacToe();
-        char[] arrayField = new char[9];
+        char[][] arrayField = new char[3][3];
+        char[] arrayJ = new char[3];
         String stringField;
         int iForArray = 0;
         boolean iBooleanXO_ = false;
@@ -16,18 +17,20 @@ public class TicTacToe {
         do {
             System.out.print("Enter cells: ");
             stringField = s.nextLine();
-            for (int i = 0; i < 9; i++) {
-                iBooleanXO_ = ticTacToe.examinationXO_(stringField.charAt(i));
-                if (iBooleanXO_) {
-                    break;
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++){
+                    iBooleanXO_ = ticTacToe.examinationXO_(stringField.charAt(i));
+                    if (iBooleanXO_) {break;}
+                    arrayField[i][j] = stringField.charAt(iForArray);
+                    iForArray++;
                 }
-                arrayField[i] = stringField.charAt(i);
+                if (iBooleanXO_) {break;}
             }
         } while (iBooleanXO_);
         System.out.println("-------\n"
-                + "|" + arrayField[0] + " " + arrayField[1] + " " + arrayField[2] + "|\n"
-                + "|" + arrayField[3] + " " + arrayField[4] + " " + arrayField[5] + "|\n"
-                + "|" + arrayField[6] + " " + arrayField[7] + " " + arrayField[8] + "|\n"
+                + "|" + arrayField[0][0] + " " + arrayField[0][1] + " " + arrayField[0][2] + "|\n"
+                + "|" + arrayField[1][0] + " " + arrayField[1][1] + " " + arrayField[1][2] + "|\n"
+                + "|" + arrayField[2][0] + " " + arrayField[2][1] + " " + arrayField[2][2] + "|\n"
                 + "-------");
     }
 
