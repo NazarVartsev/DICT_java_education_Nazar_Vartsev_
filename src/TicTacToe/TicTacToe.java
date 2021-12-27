39092e44519f972ce393ee91f0219cf2df29a511
Nazar Vartsev <n.a.vartsev@student.khai.edu>
2021-12-27 03:45:03 +0200

    TicTacToe {1} stage

---
+++ src/TicTacToe/TicTacToe.java
@@ -0,0 +1,10 @@
+package TicTacToe;
+
+public class TicTacToe {
+    public static void main(String[] args) {
+        System.out.println("""
+                X O X
+                X X X
+                O O O""");
+    }
+}
