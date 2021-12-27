1190e42b7b054750f202697abb937d7df1ef1c4d
Nazar Vartsev <n.a.vartsev@student.khai.edu>
2021-12-27 15:45:10 +0200

    WinOrLose added examinationOneElement

--- src/TicTacToe/WinOrLose.java
+++ src/TicTacToe/WinOrLose.java
@@ -1,2 +1,15 @@
-package TicTacToe;public class WinOrLose {
+package TicTacToe;
+
+public class WinOrLose {
+
+    boolean examinationOneElement(char[] arrayJ, char symbolArrayCharXO) {
+        boolean iBoolean = true;
+        boolean booleanCharField;
+        for (int j = 0; j <= 2; j++) {
+            booleanCharField = arrayJ[j] == symbolArrayCharXO;
+            iBoolean = iBoolean & booleanCharField;
+            if (!iBoolean) {return false;}
+        }
+        return true;
+    }
 }
